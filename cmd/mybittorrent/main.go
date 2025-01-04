c43b4e5bdae4283f743ec0a24d43a2686a9e3f06
Arash Outadi <arashoutadi@Arashs-MacBook-Pro.local>
2025-01-04 13:04:36 -0800

    codecrafters submit [skip ci]

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -41,13 +41,25 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 	}
 }
 
+type MetaInfoFile struct {
+	Announce string       `json:"announce"`
+	Info     MetaInfoInfo `json:"info"`
+}
+type MetaInfoInfo struct {
+	Length      int    `json:"length"`
+	Name        string `json:"name"`
+	PieceLength int    `json:"piece length"`
+	Pieces      string `json:"pieces"`
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 	command := os.Args[1]
 
-	if command == "decode" {
+	switch command {
+	case "decode":
 		bencodedValue := os.Args[2]
 		data, err := bencode.Decode(bytes.NewBufferString(bencodedValue))
 		if err != nil {
@@ -57,8 +69,27 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(data)
 		fmt.Println(string(jsonOutput))
-	} else {
+	case "info":
+		filename := os.Args[2]
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		metaInfoFile := MetaInfoFile{}
+		err = bencode.Unmarshal(file, &metaInfoFile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		jsonOutput, _ := json.Marshal(metaInfoFile)
+		fmt.Println(string(jsonOutput))
+
+	default:
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
 	}
+
 }
